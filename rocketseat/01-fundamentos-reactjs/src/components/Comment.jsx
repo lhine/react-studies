884d9dd972366e9a5bc8b8019abd6a36d6157217
lhine <amtg@cin.ufpe.br>
2024-07-03 10:27:14 -0300

    240703

--- rocketseat/01-fundamentos-reactjs/src/components/Comment.jsx
+++ rocketseat/01-fundamentos-reactjs/src/components/Comment.jsx
@@ -1,10 +1,23 @@
 /* eslint-disable react/prop-types */
+import { useState } from "react"
+import { Avatar } from "./Avatar"
+import { ThumbsUp, Trash } from "@phosphor-icons/react"
+
 import styles from "./Comment.module.css"
 
-import { ThumbsUp, Trash } from "@phosphor-icons/react"
-import { Avatar } from "./Avatar"
+export function Comment({ content, onDeleteComment }) {
+  const [likeCount, setLikeCount] = useState(0)
 
-export function Comment({ content }) {
+  function handleDeleteComment() {
+    onDeleteComment(content)
+  }
+
+  function handleLikeComment() {
+    setLikeCount((state) => {
+      return state + 1
+    })
+  }
+
   return (
     <div className={styles.comment}>
       <Avatar hasBorder={false} src="https://github.com/lhine.png" alt="" />
@@ -19,18 +32,18 @@ export function Comment({ content }) {
               </time>
             </div>
 
-            <button title="Deletar comentário">
+            <button onClick={handleDeleteComment} title="Deletar comentário">
               <Trash size={24} />
             </button>
           </header>
 
-          <p>${content}</p>
+          <p>{content}</p>
         </div>
 
         <footer>
-          <button>
+          <button onClick={handleLikeComment}>
             <ThumbsUp />
-            Aplaudir <span>20</span>
+            Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>
--- rocketseat/01-fundamentos-reactjs/src/components/Post.jsx
+++ rocketseat/01-fundamentos-reactjs/src/components/Post.jsx
@@ -34,8 +34,23 @@ export function Post({ author, publishedAt, content }) {
   }
 
   function handleNewCommentChange(event) {
+    event.target.setCustomValidity("")
     setNewCommentText(event.target.value)
   }
+
+  function handleNewCommentInvalid(event) {
+    event.target.setCustomValidity("Esse campo é obrigatório")
+  }
+
+  function deleteComment(commentToDelete) {
+    const commentsWithoutDeletedOne = comments.filter((comment) => {
+      return comment !== commentToDelete
+    })
+
+    setComments(commentsWithoutDeletedOne)
+  }
+
+  const isNewCommentEmpty = newCommentText.length === 0
 
   return (
     <article className={styles.post}>
@@ -78,16 +93,26 @@ export function Post({ author, publishedAt, content }) {
           placeholder="Deixe um comentário"
           value={newCommentText}
           onChange={handleNewCommentChange}
+          onInvalid={handleNewCommentInvalid}
+          required
         />
 
         <footer>
-          <button type="submit">Publicar</button>
+          <button type="submit" disabled={isNewCommentEmpty}>
+            Publicar
+          </button>
         </footer>
       </form>
 
       <div className={styles.commentList}>
         {comments.map((comment) => {
-          return <Comment key={comment} content={comment} />
+          return (
+            <Comment
+              key={comment}
+              content={comment}
+              onDeleteComment={deleteComment}
+            />
+          )
         })}
       </div>
     </article>
