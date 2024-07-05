f062230d04ccd7ffb76c2a939d8e11e6ec28f9e5
lhine <amtg@cin.ufpe.br>
2024-07-05 10:32:12 -0300

    240705

---
+++ rocketseat/02-ignite-timer/src/App.tsx
@@ -0,0 +1,18 @@
+import { ThemeProvider } from 'styled-components'
+import { Button } from './components/Button'
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStype } from './styles/global'
+
+export function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="danger" />
+      <Button variant="success" />
+      <Button />
+
+      <GlobalStype />
+    </ThemeProvider>
+  )
+}
